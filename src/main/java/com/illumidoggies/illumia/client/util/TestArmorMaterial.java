c2677cffb555673d45e392e35cebeef1758b3598
RaygosAscend <alanildephonsesheen@gmail.com>
2021-02-14 01:22:55 -0700

    no major improvements, spacing fixes

--- src/main/java/com/illumidoggies/illumia/client/util/TestArmorMaterial.java
+++ src/main/java/com/illumidoggies/illumia/client/util/TestArmorMaterial.java
@@ -14,6 +14,7 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import java.util.function.Supplier;
 
 public enum TestArmorMaterial implements IArmorMaterial {
+	
     TEST(Illlumia.MOD_ID + ":test", 20, new int[]{4, 7, 9, 4}, 50, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND,
             3.0F, () -> { return Ingredient.fromItems(ItemInit.special_item.get()); });
 
